48f63ed4eedbe24f0df4a567813a218ee6ec5021
Chris Bass <118141481+christopherb-pearfiction@users.noreply.github.com>
2024-09-14 20:17:45 -0400

    init

---
+++ astro.config.mjs
@@ -0,0 +1,49 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightImageZoom from 'starlight-image-zoom'
+import tailwind from '@astrojs/tailwind';
+
+import vercel from '@astrojs/vercel/serverless';
+
+export const locales = {
+    en: { label: 'English', lang: 'en' },
+    fr: { label: 'Français', lang: 'fr' },
+};
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight({
+      title: 'Elizabeth Whalley',
+         // Set English as the default language for this site.
+         defaultLocale: 'en',
+         locales,
+      social: {
+          instagram: 'https://www.instagram.com/elizabethwhalleyprojects/',
+      },
+      plugins: [starlightImageZoom()],
+      customCss: [
+          // Relative path to your custom CSS file
+          './src/styles/custom.css',
+        ],
+      sidebar: [
+          {
+              label: 'Group Shows',
+              translations: {
+                  fr: 'Expositions Collectives',
+              },
+              autogenerate: { directory: 'group shows' },
+          },
+          {
+              label: 'Solo Shows',
+              translations: {
+                  fr: 'Expositions Solo',
+              },
+              autogenerate: { directory: 'reference' },
+          },
+      ],
+      }), tailwind()],
+
+  output: 'server',
+  adapter: vercel(),
+});
